10eb634a4c8c4439e368bd3074e52e4470716e50
Volodymyr Baisa <biosvova@gmail.com>
2021-10-30 00:10:27 -0400

    project

---
+++ src/test/java/com/calculator/vbaisa/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.vbaisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
