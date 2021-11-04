02de235cf59191419eb77b0602413375ae3c425d
Volodymyr Baisa <biosvova@gmail.com>
2021-11-03 23:06:19 -0400

    Add Service a code for calculate value to Controller. Close #7

--- src/main/java/com/calculator/vbaisa/controller/v1/CalcController.java
+++ src/main/java/com/calculator/vbaisa/controller/v1/CalcController.java
@@ -1,5 +1,7 @@
 package com.calculator.vbaisa.controller.v1;
 
+import com.calculator.vbaisa.service.CalcService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/v1/")
 public class CalcController {
 
+    @Autowired
+    private CalcService calcService;
+
     @GetMapping("calculate/{exp}")
     public @ResponseBody String calculateResult(@PathVariable String exp) {
-        return "";
+        return calcService.CalculateValue(exp);
     }
 }
